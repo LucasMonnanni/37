b8d60f8aa5d37494f5eca447ac969a33247501fa
LucasMonnanni <lucasmonnanni@gmail.com>
2020-04-26 17:49:12 -0300

    Reparte bien, luchando para jugar la primera carta

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
+from operator import itemgetter
 
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'vivaperoncarajo'
@@ -44,13 +45,14 @@ class Game(Document):
     { 'teamA': {'player1': {'username': None, 'hand': []}, \
                 'player2': {'username': None, 'hand': []}, \
                 'full': False, 'score': 0, 'winner': False}, \
-      'teamA': {'player1': {'username': None, 'hand': []}, \
+      'teamB': {'player1': {'username': None, 'hand': []}, \
                 'player2': {'username': None, 'hand': []}, \
                 'full': False, 'score': 0, 'winner': False}})
+    current_player = fields.DictField(default={'team': None, 'player': None, 'n': None})
     full = fields.BoolField(default=False)
     start = fields.DateTimeField()
     end = fields.DateTimeField()
-    players = property(get_players)
+
 
     def get_players(self):
         players = []
@@ -59,47 +61,63 @@ class Game(Document):
                 players.append(self.teams[t][p]['username'])
         return players
 
+    players = property(get_players)
 
+    def find_player(self, username):
+            for team in ['teamA', 'teamB']:
+                for player in ['player1', 'player2']:
+                    if self.teams[team][player]['username'] == username:
+                        return {'team': team, 'player': player}
 
     def add_player(self, username, team):
-        if username in self.get_players():
+        if username in self.players:
             find_player(self, username)
         else:
             if self.full:
                 return None
             else:
                 if self.teams[team]['player1']['username'] == None:
-                    self.teams[team]['player1']['username'] = username
+                    teams = self.teams
+                    teams[team]['player1']['username'] = username
+                    self.teams = teams
+                    self.commit()
                     return {'team': team, 'player': 'player1'}
                 else:
-                    self.teams[team]['player2']['username'] = username
+                    teams = self.teams
+                    teams[team]['player2']['username'] = username
+                    teams[team]['full'] = True
+                    self.teams = teams
+                    self.commit()
+                    if self.teams['teamA']['full'] and self.teams['teamB']['full']:
+                        self.full = True
+                    self.commit()
                     return {'team': team, 'player': 'player2'}
-                    self.teams[team]['full'] = True
-                    if self.teams['teamA']['full'] and self['teams']['teamB']['full']:
-                        self['full'] = True
 
     def del_player(self, team, player):
+        teams = self.teams
         if player == 'player1':
             self.teams[team][player]['username'] = self.teams[team]['player2']['username']
             self.teams[team]['full'] = False
-        self.teams[team]['player2']['username'] = None
-
-    def find_player(self, username):
-        if username in self.players:
-            for team in ['teamA', 'teamB']:
-                for player in ['player1', 'player2']:
-                    if self.teams[team][player]['username'] == username:
-                        return {'team': team, 'player': player}
-                        break
+        teams[team]['player2']['username'] = None
+        self.teams = teams
+        self.commit()
 
     def deal(self):
         open_deck = deck
+        teams = self.teams
         for t in ['teamA','teamB']:
             for p in ['player1', 'player2']:
+                h = []
                 for i in range(10):
                     card = random.choice(open_deck)
                     open_deck.remove(card)
-                    self.teams[t][p]['hand'].append(card))
+                    h.append(card)
+                h.sort(key=lambda c: order.index(c[0]))
+                h.sort(key=lambda c: c[1])
+                hand = [[i]+c for i,c in enumerate(h)]
+                teams[t][p]['hand'] = hand
+        self.teams = teams
+        self.commit()
 
 @login.user_loader
 def load_user(username):
@@ -107,6 +125,10 @@ def load_user(username):
 
 @app.before_first_request
 def before_first_request_func():
+    global order
+    order = ['3', '2', '1', '10', '9', '8', '7', '6', '5', '4']
+    global value
+    value = {'1':3, '2':1, '3':1, '10':1, '9':1, '8':1, '7':0, '6':0, '5':0, '4':0}
     global open_game
     open_game = Game()
     open_game.commit()
@@ -114,7 +136,10 @@ def before_first_request_func():
     deck = []
     for n in range(10):
         for p in ['basto', 'copa', 'espada', 'oro']:
-            deck.append([n+1 , p])
+            deck.append([str(n+1) , p])
+    global current_player
+    global players_order
+    players_order = [('teamB', 'player2'), ('teamB', 'player1'), ('teamA', 'player2'), ('teamA', 'player1')]
     print('Before')
 
 
@@ -165,7 +190,7 @@ def main():
     else:
         return flask.render_template('main.html')
 
-@socketio.on('connect')
+@socketio.on('connected')
 def connect():
     player_data = open_game.find_player(current_user.username)
     if player_data != None:
@@ -175,15 +200,18 @@ def connect():
 
 @socketio.on('player_enter')
 def player_enter(data):
-    player_data = open_game.add_player(current_user.username, data.team)
-    open_game.commit()
-    if data == None:
+    player_data = open_game.add_player(current_user.username, data['team'])
+    if player_data == None:
         emit('game_full')
     else:
         emit('player_data', player_data)
         socketio.sleep(0)
+        print('Seguimos..')
         if open_game.full:
             open_game.deal()
+            current_player = {'team': random.choice(['teamA', 'teamB']), 'player': random.choice(['player1', 'player2']), 'n': 1}
+            open_game.current_player = current_player
+            open_game.start = datetime.datetime.now()
             open_game.commit()
             emit('game_starts', open_game.dump(), broadcast=True)
         else:
@@ -191,6 +219,11 @@ def player_enter(data):
 
 @socketio.on('player_left')
 def player_left(player_data):
-    open_game.del_player(player_data.team, player_data.player)
+    open_game.del_player(player_data['team'], player_data['player'])
     open_game.commit()
     emit('players_update', open_game.dump(), broadcast=True)
+
+@socketio.on('play_card')
+def card_played(data):
+    card_data = {'username': open_game.teams[data['team']][data['player']]['username'], 'card': data['card']}
+    emit('card_played', data)
