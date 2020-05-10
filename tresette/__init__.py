6448674ee3edc6940a839f685d5b82dea5524f95
LucasMonnanni <lucasmonnanni@gmail.com>
2020-05-09 22:53:47 -0300

    Bugfix

--- tresette/__init__.py
+++ tresette/__init__.py
@@ -8,8 +8,8 @@ app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'vivaperoncarajo'
 socketio = SocketIO(app)
 login = LoginManager(app)
-# db = MongoClient("mongodb+srv://moctezuma:4KTRUl2bvvmK1KGX@cluster0-zhilx.gcp.mongodb.net/test?retryWrites=true&w=majority").tresette
-db = MongoClient().tresette
+db = MongoClient("mongodb+srv://moctezuma:4KTRUl2bvvmK1KGX@cluster0-zhilx.gcp.mongodb.net/test?retryWrites=true&w=majority").tresette
+# db = MongoClient().tresette
 instance = Instance(db)
 
 import tresette.views
